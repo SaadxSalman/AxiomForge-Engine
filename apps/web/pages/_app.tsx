17d6b5e6f151126a381ed60c54676c1286b9e883
saadsalmanakram <154062428+saadsalmanakram@users.noreply.github.com>
2025-12-25 15:49:01 +0500

    Create _app.tsx

---
+++ apps/web/pages/_app.tsx
@@ -0,0 +1,9 @@
+import { trpc } from '../utils/trpc';
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default trpc.withTRPC(MyApp);
