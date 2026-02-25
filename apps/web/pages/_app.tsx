92f785fd2dfd6ffbc3537399cc0feed143325de5
saadsalmanakram <154062428+SaadxSalman@users.noreply.github.com>
2026-02-25 09:21:50 +0500

    update

--- apps/web/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import { trpc } from '../utils/trpc';
-import type { AppProps } from 'next/app';
-import '../styles/globals.css';
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default trpc.withTRPC(MyApp);
